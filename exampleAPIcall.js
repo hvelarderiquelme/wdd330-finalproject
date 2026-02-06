9861f1b939667a5d9144b5cc427a747c7b85988f
hvelarderiquelme <hvelardeab@gmail.com>
2026-02-05 20:56:53 -0700

    fix absolute paths

--- exampleAPIcall.js
+++ exampleAPIcall.js
@@ -1,10 +1,11 @@
 // Replace with your API key
-const apiKey = "10b77d84322cfebe7b2e39b93d5e71ae";
+//const apiKey = "10b77d84322cfebe7b2e39b93d5e71ae";
 
 // Example: Get leagues
  //const apiUrl = "https://v3.football.api-sports.io/teams?id=50";
  //const apiUrl = `https://newsapi.org/v2/everything?q=Premier+League+OR+EPL+soccer&sortBy=publishedAt&language=en&apiKey=8392875616ea461980cce98e14fe73a8`;
  //const apiUrl = 'https://v3.football.api-sports.io/teams/statistics?team=42&league=39&season=2024';
+ 
  const headers = {
    "x-apisports-key": apiKey,
    "Accept": "application/json"
